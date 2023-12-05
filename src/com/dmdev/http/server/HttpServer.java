072e881aff28ac2a3ae74eef8d2c8154e4a2a1ed
Ivan Khizhkin <ivan.khizhkin@gmail.com>
2023-12-05 09:34:31 +0300

    Single threaded server

---
+++ src/com/dmdev/http/server/HttpServer.java
@@ -0,0 +1,47 @@
+package com.dmdev.http.server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class HttpServer {
+    private final int port;
+
+    public HttpServer(int port){
+        this.port=port;
+    }
+
+    public void run(){
+        try {
+            var server = new ServerSocket(port);
+            var socket = server.accept();
+            getProcessSocket(socket);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void getProcessSocket(Socket socket) {
+        try(socket;
+            var inputStream = new DataInputStream(socket.getInputStream());
+            var outputStream = new DataOutputStream(socket.getOutputStream())) {
+            System.out.println("Request: " + new String(inputStream.readNBytes(400)));
+
+            var body = Files.readAllBytes(Path.of("resources", "example.html"));
+            var headers = """
+                    HTTP/1.1 200 OK
+                    content-typpe: text/html
+                    content-length: %s
+                    """.formatted(body.length).getBytes();
+            outputStream.write(headers);
+            outputStream.write(System.lineSeparator().getBytes());
+            outputStream.write(body);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}

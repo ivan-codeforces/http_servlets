b8255dd8d31e3883cf8e8f1625435fef939d92be
Ivan Khizhkin <ivan.khizhkin@gmail.com>
2023-12-05 10:05:20 +0300

    Thread pool server

--- src/com/dmdev/http/server/HttpClientRunner.java
+++ src/com/dmdev/http/server/HttpClientRunner.java
@@ -8,9 +8,10 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpRequest.BodyPublishers;
 import java.net.http.HttpResponse;
 import java.nio.file.Path;
+import java.util.concurrent.ExecutionException;
 
 public class HttpClientRunner {
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws IOException, InterruptedException, ExecutionException {
         var httpClient = HttpClient.newBuilder().version(HttpClient.Version.HTTP_1_1).build();
 
         var request = HttpRequest.newBuilder()
@@ -19,8 +20,11 @@ public class HttpClientRunner {
                 .POST(BodyPublishers.ofFile(Path.of("resources", "first.json")))
                 .build();
 
-        var response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(response.headers());
-        System.out.println(response.body());
+        var response = httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString());
+        var response2 = httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString());
+        var response3 = httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString());
+//        System.out.println(response.headers());
+//        System.out.println(response.body());
+        System.out.println(response3.get().body());
     }
 }
--- src/com/dmdev/http/server/HttpServer.java
+++ src/com/dmdev/http/server/HttpServer.java
@@ -7,19 +7,27 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class HttpServer {
-    private final int port;
 
-    public HttpServer(int port){
+    private final ExecutorService pool;
+    private final int port;
+    private boolean stopped;
+
+    public HttpServer(int port, int poolSize){
         this.port=port;
+        this.pool = Executors.newFixedThreadPool(poolSize);
     }
 
     public void run(){
         try {
             var server = new ServerSocket(port);
-            var socket = server.accept();
-            getProcessSocket(socket);
+            while (!stopped){
+                var socket = server.accept();
+                pool.submit(()->getProcessSocket(socket));
+            }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -43,5 +51,9 @@ public class HttpServer {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public void setStopped(boolean stopped) {
+        this.stopped = stopped;
     }
 }

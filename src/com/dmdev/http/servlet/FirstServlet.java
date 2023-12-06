1bcd7a356c586b66690773c7494d922b4d38faad
Ivan Khizhkin <ivan.khizhkin@gmail.com>
2023-12-06 14:53:37 +0300

    post request

---
+++ src/com/dmdev/http/servlet/FirstServlet.java
@@ -0,0 +1,52 @@
+package com.dmdev.http.servlet;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.Map;
+
+@WebServlet("/first")
+public class FirstServlet extends HttpServlet {
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getHeader("user-agent");
+        var paramValue = req.getParameter("param");
+        var parameterMap = req.getParameterMap();
+
+        var headerNames = req.getHeaderNames();
+        while (headerNames.hasMoreElements()){
+            var header = headerNames.nextElement();
+            System.out.println(req.getHeader(header));
+        }
+        resp.setContentType("text/html; charset=UTF-8");
+        resp.setHeader("token", "12345");
+        try (var writer = resp.getWriter()) {
+            writer.write("<h1>Привет<h1>");
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        var parameterMap = req.getParameterMap();
+        System.out.println(parameterMap);
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+    }
+
+
+}
